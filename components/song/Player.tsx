ea9dd3fedf6f6de671bf9cdbd8901699f94a3b7e
santoshkamble8168 <santoshkamble8168@gmil.com>
2023-09-02 19:52:16 +0530

    song undefined state handled

--- components/song/Player.tsx
+++ components/song/Player.tsx
@@ -20,7 +20,7 @@ const Player = () => {
 
 	return (
 		<div className="fixed bottom-0 bg-black w-full py-2 h-[80px] px-4">
-			<PlayerContent key={songUrl} song={song} songUrl={songUrl} />
+			{song && (<PlayerContent key={songUrl} song={song} songUrl={songUrl} />)}
 		</div>
 	);
 };

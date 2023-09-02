a3faee757e3b092fab970449fd44652395049e1c
santoshkamble8168 <santoshkamble8168@gmil.com>
2023-09-02 19:57:04 +0530

    stripe apiversion updated

--- libs/stripe.ts
+++ libs/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from "stripe";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? "", {
-	apiVersion: "2023-08-16",
+	apiVersion: "2022-11-15",
 	appInfo: {
 		name: "Spotify Clone Audio System",
 		version: "0.1.0",
